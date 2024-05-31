79eee7c1a9089385d1e1837e8663d33b17597ac8
Amogh Maroju <amogh.gyana@gmail.com>
2024-05-31 18:22:47 -0500

    Routing WITH  react routerclearclear

--- personal-portfolio/src/App.tsx
+++ personal-portfolio/src/App.tsx
@@ -1,27 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Link } from "react-router-dom";
 import AboutMe from "./AboutMe";
 import HomePage from "./HomePage";
 import NavBar from "./NavBar";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  let component; //: (() => JSX.Element) | undefined;
-  switch (window.location.pathname) {
-    case "/":
-      component = <HomePage />;
-      break;
-    case "/AboutMe":
-      component = <AboutMe />;
-      break;
-    default:
-      break;
-  }
+  // let component; //: (() => JSX.Element) | undefined;
+  // switch (window.location.pathname) {
+  //   case "/":
+  //     component = <HomePage />;
+  //     break;
+  //   case "/AboutMe":
+  //     component = <AboutMe />;
+  //     break;
+  //   default:
+  //     break;
+  // }
 
   return (
     <>
       <NavBar />
-      {component}
+
+      {/* Pages */}
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/AboutMe" element={<AboutMe />} />
+        </Routes>
+      </div>
     </>
   );
 };
---
+++ personal-portfolio/src/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        Home
+      </Link>
+      <ul>
+        <li>
+          <Link to="/AboutMe">About Me</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
