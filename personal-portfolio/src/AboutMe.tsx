f8b9aa7f1141dcb542b83ab3c7900e776f6984c5
Amogh Maroju <amogh.gyana@gmail.com>
2024-06-02 00:37:21 -0500

    Added more to about me + minor formatting

--- personal-portfolio/src/AboutMe.tsx
+++ personal-portfolio/src/AboutMe.tsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <div>
+    <div className="about-me">
       <h1>About Me!!!</h1>
+      <h3>
+        Hi, I'm Amogh Maroju, and I undergraduate Computer Science student at
+        The University of Texas at Austin. I have a passion for and love
+        learning more about cybersecurity, AI/ML, and software development.
+      </h3>
+      <h3>
+        <a href="https://www.linkedin.com/in/amoghmaroju">LinkedIn</a>
+      </h3>
+      <h3>
+        <a href="https://github.com/amaro52">GitHub</a>
+      </h3>
     </div>
   );
 };
