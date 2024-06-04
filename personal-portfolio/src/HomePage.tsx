96d55683047cd6d19f2e3c2f1812c4a01f0b7c14
Amogh Maroju <amogh.gyana@gmail.com>
2024-06-04 11:22:15 -0500

    added cards for each prject

--- personal-portfolio/src/HomePage.tsx
+++ personal-portfolio/src/HomePage.tsx
@@ -1,39 +1,55 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div className="px-4">
-      <p className="uppercase text-4xl font-semibold font-serif">Projects</p>
+    <div className="p-5">
+      <p className=" text-4xl font-semibold font-serif">My Projects</p>
 
-      <button className="font-serif inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+      <button className="font-serif cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
         Button
       </button>
 
-      <div className="collapse collapse-arrow bg-base-200">
-        <input type="radio" name="my-accordion-2" defaultChecked />
-        <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
+      <div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl inline-block p-4">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Custom Shuffle</h2>
+            <p>Description</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">
+                <Link
+                  to="https://github.com/amaro52/spotify-custom-shuffle"
+                  target="_blank"
+                >
+                  GitHub Repo
+                </Link>
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="collapse-content">
-          <p>hello</p>
-        </div>
-      </div>
-      <div className="collapse collapse-arrow bg-base-200">
-        <input type="radio" name="my-accordion-2" />
-        <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
-        </div>
-        <div className="collapse-content">
-          <p>hello</p>
-        </div>
-      </div>
-      <div className="collapse collapse-arrow bg-base-200">
-        <input type="radio" name="my-accordion-2" />
-        <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
-        </div>
-        <div className="collapse-content">
-          <p>hello</p>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl inline-block p-4">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Chariot.</h2>
+            <p>Description</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">
+                <Link to="https://github.com/amaro52chariot." target="_blank">
+                  GitHub Repo
+                </Link>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
